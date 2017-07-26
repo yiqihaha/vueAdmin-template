d69d29d3e07723d0955e1509a07b186c7877c5ef
edwin.wang <edwin.wang@centling.com>
2017-07-26 15:17:56 +0800

    Part1

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,31 +1,36 @@
 <template>
-    <div class="dashboard-container">
-        <div class='dashboard-text'>name:{{name}}</div>
-        <div class='dashboard-text'>role:<span v-for='role in roles' :key='role'>{{role}}</span></div>
-    </div>
+  <div class="dashboard-container">
+    <component v-bind:is="currentRole"> </component>
+  </div>
 </template>
 
 <script>
-    import { mapGetters } from 'vuex';
-    export default {
-        name: 'dashboard',
-        computed: {
-            ...mapGetters([
-                'name',
-                'roles'
-            ])
-        }
-    }
-</script>
+  import { mapGetters } from 'vuex';
+  import EditorDashboard from './editor/index';
+  import DefaultDashboard from './default/index';
 
-<style rel="stylesheet/scss" lang="scss">
-    .dashboard {
-        &-container {
-            margin: 30px;
-        }
-        &-text {
-            font-size: 30px;
-            line-height: 46px;
-        }
+  export default {
+    name: 'dashboard',
+    components: { EditorDashboard, DefaultDashboard },
+    data() {
+      return {
+        currentRole: 'EditorDashboard'
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'name',
+        'avatar',
+        'email',
+        'introduction',
+        'roles'
+      ])
+    },
+    created() {
+      if (this.roles.indexOf('admin') >= 0) {
+        return;
+      }
+      this.currentRole = 'DefaultDashboard';
     }
-</style>
+  }
+</script>
